c9249abcfebbb53b2ae0d81626f4b7e76b01f959
saeedmohammadiha <saeed1440124@gmail.com>
2023-08-16 23:53:37 +0330

    complete layput and make it responsive and complete navbar

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,18 +1,40 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./style.module.scss";
 import logo from "../../assets/icons/logo.svg";
+import profileIcon from "../../assets/icons/user.svg";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
 export default function Navbar() {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const name = localStorage.getItem("name");
+    name && setName(name);
+  }, []);
+
   return (
     <navbar className={styles.navbar}>
       <ul>
         <li>
-          <Image src={logo} alt="logo" />
+          <Link href={"/"}>
+            <Image src={logo} alt="Logo" />
+          </Link>
         </li>
         <li>
-          <h3>سامانه ثبت نام خوردد</h3>
+          <h3>سامانه مقایسه و خرید آنلاین بیمه</h3>
         </li>
         <li>
-          <a>ثبت نام</a>
+          {name ? (
+            <div className={styles.username}>
+              <Image src={profileIcon} alt="Profile Icon" />
+              <span> {name}</span>
+            </div>
+          ) : (
+            <Link href={"/"}>ثبت نام</Link>
+          )}
         </li>
       </ul>
     </navbar>
